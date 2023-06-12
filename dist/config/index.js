f38304d8544518df905ccbb50bdc6b5cfa873f97
Jalenzl <563344794@qq.com>
2023-06-12 10:20:54 +0800

    version 0612 add gitignore

--- dist/config/index.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.systemConfig = void 0;
-const systemConfig = {
-    port: 8000,
-};
-exports.systemConfig = systemConfig;
