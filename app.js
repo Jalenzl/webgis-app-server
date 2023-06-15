a158df2884ba7404af906810aabdb802b3cdacad
Jalenzl <563344794@qq.com>
2023-06-15 14:49:33 +0800

    version 0615 test cors

--- app.js
+++ app.js
@@ -3,6 +3,7 @@ const express = require('express');
 const path = require('path');
 const cookieParser = require('cookie-parser');
 const logger = require('morgan');
+const cors = require('cors');
 
 const indexRouter = require('./routes/index');
 const usersRouter = require('./routes/users');
@@ -19,10 +20,13 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
+app.use(cors());
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
 app.use('/api/v1/cesium', cesiumRouter);
+
+const port = process.env.PORT || 3000;
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
