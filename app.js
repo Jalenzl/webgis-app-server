a9a7471cdaf23f7678e7c0509ba9c7efa4b54dca
Jalenzl <563344794@qq.com>
2023-06-21 11:22:58 +0800

    version 0621 try to fix coor bugs

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const createError = require('http-errors');
 const express = require('express');
 const path = require('path');
 const cookieParser = require('cookie-parser');
-const bodyParser= require('body-parser');
+// const bodyParser= require('body-parser');
 const logger = require('morgan');
 const cors = require('cors');
 
@@ -19,11 +19,11 @@ app.set('view engine', 'jade');
 
 app.use(cors());
 app.use(logger('dev'));
-app.use(express.json());
-app.use(express.urlencoded({ extended: false }));
+app.use(express.json({limit: '50mb'}));
+app.use(express.urlencoded({limit: '50mb', extended: true}));
 app.use(cookieParser());
-app.use(bodyParser.json({limit: '50mb'}));
-app.use(bodyParser.urlencoded({limit: '50mb', extended: true}));
+// app.use(bodyParser.json({limit: '50mb'}));
+// app.use(bodyParser.urlencoded({limit: '50mb', extended: true}));
 app.use(express.static(path.join(__dirname, 'public')));
 
 
